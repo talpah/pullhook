587037effa7b90476363071f079962c937e0e703
Cosmin <talpah@gmail.com>
2014-09-09 11:19:23 +0300

    Config file is now optional; Replaced print statements with logging.debug

--- pullhook.py
+++ pullhook.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 """
-Github hook for deployment
+    Github hook for deployment
+        Feel free to bind the server to any ip and port you desire
+        just make sure it's accessible by GitHub
 """
 from ConfigParser import SafeConfigParser
+import logging
 
 __author__ = 'talpah@gmail.com'
 
@@ -11,12 +14,15 @@ from bottle import route, request, run
 import git
 import os
 
+
+MYDIR = os.path.dirname(os.path.abspath(__file__))
+
 """
  DEFAULT CONFIG VALUES begin
  Use config.ini to override
 """
 DEFAULT_CONFIG = {
-    'basedir': os.path.dirname(os.path.realpath(__file__)),  # Default path for git pull
+    'basedir': MYDIR,  # Default path for git pull
     'listen_host': '0.0.0.0',  # IP to listen on; Defaults to all interfaces
     'listen_port': '7878',  # Port to listen to
 }
@@ -29,10 +35,15 @@ def init():
     Read config file
 
     """
-    parser = SafeConfigParser(DEFAULT_CONFIG)
-    parser.read(os.path.dirname(os.path.abspath(__file__)) + '/config.ini')
     global CONFIG
-    CONFIG = dict(parser.items('pullhook'))
+    config_file = os.path.join(MYDIR, 'config.ini')
+    if not os.path.isfile(config_file):
+        CONFIG = DEFAULT_CONFIG
+        logging.info('No config file found at "%s", using defaults' % config_file)
+    else:
+        parser = SafeConfigParser(DEFAULT_CONFIG)
+        parser.read(config_file)
+        CONFIG = dict(parser.items('pullhook'))
 
 
 @route('/', method=['POST'])
@@ -43,25 +54,21 @@ def handle_payload():
     """
     data = request.json
     event = request.get_header('X-GitHub-Event')
-    print "Received event: %s" % event
+    logging.debug("Received event: %s" % event)
     if event == 'push':
         """ Parse branch name from push data ref """
         pushed_branch = data['ref'].split('/')[-1]
-        print "Received push event in branch %s" % pushed_branch
+        logging.debug("Received push event in branch %s" % pushed_branch)
         """ Use GitPython """
         g = git.Git(CONFIG['basedir'])
         """ Get active branch """
         branch = g.rev_parse('--abbrev-ref', 'HEAD')
-        print "We're on branch %s" % branch
+        logging.debug("We're on branch %s" % branch)
         if pushed_branch == branch:
-            print "Pulling repo in %s" % CONFIG['basedir']
+            logging.debug("Pulling repo in %s" % CONFIG['basedir'])
             g.pull()
 
 
 if __name__ == '__main__':
-    """
-    Feel free to bind the server to any ip and port you desire
-    just make sure it's accessible by GitHub
-    """
     init()
     run(host=CONFIG['listen_host'], port=CONFIG['listen_port'], debug=True)
