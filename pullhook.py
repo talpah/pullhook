1ac3d27f387093b1965eaafa78bd2e7800b33e83
Cosmin <talpah@gmail.com>
2014-09-10 11:05:14 +0300

    Single instance; Better logging handling; Defaults to quiet operating mode

--- pullhook.py
+++ pullhook.py
@@ -10,7 +10,8 @@ import logging
 
 __author__ = 'talpah@gmail.com'
 
-from bottle import route, request, run, abort
+import bottle
+from tendo import singleton
 import git
 import os
 
@@ -22,12 +23,28 @@ MYDIR = os.path.dirname(os.path.abspath(__file__))
  Use config.ini to override
 """
 DEFAULT_CONFIG = {
+    'debug': 'false',  # Single instance
     'listen_host': '0.0.0.0',  # IP to listen on; Defaults to all interfaces
     'listen_port': '7878',  # Port to listen to
 }
 
 MAIN_CONFIG = {}
 REPOS_CONFIG = {}
+
+"""
+    Single instance
+    Hide warning to prevent cron mails
+"""
+tmp = logging.getLogger().level
+logging.getLogger().setLevel(logging.CRITICAL)
+_myinstance_ = singleton.SingleInstance()
+logging.getLogger().setLevel(tmp)
+
+""" Logging """
+logger = logging.getLogger("pullhook")
+logger.addHandler(logging.StreamHandler())
+
+""" Code below """
 
 
 def init():
@@ -43,6 +60,8 @@ def init():
         parser = SafeConfigParser()
         parser.read(config_file)
         MAIN_CONFIG = dict(DEFAULT_CONFIG.items() + dict(parser.items('pullhook')).items())
+        if MAIN_CONFIG['debug'].lower() in ['true', 'yes', '1', 'on']:
+            logger.setLevel(logging.DEBUG)
         sections = parser.sections()
         if len(sections) < 2:
             raise Exception('No applications defined. See "config.ini.sample" for howto.')
@@ -50,44 +69,57 @@ def init():
         for repo in sections:
             if repo != 'pullhook':
                 REPOS_CONFIG[repo] = dict(parser.items(repo))
-        logging.debug('Found %d repos in config.' % len(REPOS_CONFIG))
+        logger.debug('Found %d repos in config.' % len(REPOS_CONFIG))
 
 
-@route('/', method=['POST'])
+@bottle.route('/', method=['POST'])
 def handle_payload():
     """
     Handles hook requests
 
     """
-    data = request.json
-    event = request.get_header('X-GitHub-Event')
-    logging.debug("Received event: %s" % event)
+    data = bottle.request.json
+    event = bottle.request.get_header('X-GitHub-Event')
+    logger.debug("Received event: %s" % event)
     if event == 'push':
         """ Parse repo name from push data ref """
         pushed_repo = data['repository']['name']
         """ Parse branch name from push data ref """
         pushed_branch = data['ref'].split('/')[-1]
-        logging.debug("Received push event from repo %s in branch %s" % (pushed_repo, pushed_branch))
+        logger.debug("Received push event from repo %s in branch %s" % (pushed_repo, pushed_branch))
 
         if pushed_repo not in REPOS_CONFIG:
-            logging.warning('Repo "%s" is not configured on our end. Skipping.' % pushed_repo)
-            abort(404, 'Not configured: %s' % pushed_repo)
+            logger.warning('Repo "%s" is not configured on our end. Skipping.' % pushed_repo)
+            bottle.abort(404, 'Not configured: %s' % pushed_repo)
         configured_repo = REPOS_CONFIG[pushed_repo]
 
         """ Use GitPython """
         g = git.Git(configured_repo['basedir'])
         """ Get active branch """
         branch = g.rev_parse('--abbrev-ref', 'HEAD')
-        logging.debug("Repo \"%s\", branch \"%s\"" % (configured_repo['basedir'], branch))
+        logger.debug("Repo \"%s\", branch \"%s\"" % (configured_repo['basedir'], branch))
         if pushed_branch == branch:
-            logging.debug("Pulling new commits.")
+            logger.debug("Pulling new commits.")
             g.pull()
         else:
-            logging.debug('Branches are not corresponding: "%s" (local) and "%s" (remote). Skipping.' % (branch, pushed_branch))
+            logger.debug(
+                'Branches are not corresponding: "%s" (local) and "%s" (remote). Skipping.' % (branch, pushed_branch))
             # no abort here
 
 
 if __name__ == '__main__':
-    # logging.basicConfig(level=logging.DEBUG)  # Uncomment this for debug
     init()
-    run(host=MAIN_CONFIG['listen_host'], port=MAIN_CONFIG['listen_port'], debug=True)
+
+    """ Bottle arguments """
+    bottle_params = {
+        'host': MAIN_CONFIG['listen_host'],
+        'port': MAIN_CONFIG['listen_port'],
+        'quiet': True
+    }
+
+    """ DEBUG """
+    if MAIN_CONFIG['debug'].lower() in ['true', 'yes', '1', 'on']:
+        bottle_params['quiet'] = False
+        bottle_params['debug'] = True
+
+    bottle.run(**bottle_params)
