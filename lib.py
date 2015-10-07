73cc2c7253f1c75f63e4c73084205f5dbab6c83a
Cosmin <talpah@gmail.com>
2015-10-07 21:14:38 +0300

    New version

---
+++ lib.py
@@ -0,0 +1,228 @@
+# coding=utf-8
+"""
+Lib
+"""
+import logging
+from subprocess import CalledProcessError, STDOUT
+
+from git.repo import Repo
+import os
+import bottle
+from slugify import slugify
+import sys
+import yaml
+
+REPOS = {}
+AUTO_REPOS = {}
+
+
+class Config(dict):
+    """
+    Config file as class attributes
+    """
+
+    def __getattr__(self, name):
+        value = self[name]
+        if isinstance(value, dict):
+            value = Config(value)
+        return value
+
+
+logger = logging.getLogger("pullhook")
+logger.addHandler(logging.StreamHandler())
+
+CONFIG_FILE = os.path.realpath(os.path.join(os.path.dirname(__file__), 'config.yml'))
+if not os.path.exists(CONFIG_FILE):
+    logger.fatal("Can't find a config file. Copy 'config.yml.sample' as 'config.yml' and start editing.")
+    sys.exit(10)
+with open(CONFIG_FILE) as fp:
+    parsed_config = yaml.load(fp)
+try:
+    config = Config(parsed_config)
+except TypeError:
+    logger.fatal("Invalid config.yml. Use 'config.yml.sample' as guide.")
+    sys.exit(20)
+
+if 'debug' in config and config.debug:
+    logger.setLevel(logging.DEBUG)
+
+
+def run_application():
+    """
+        Pre-flight checks and run_application
+    """
+
+    try:
+        """ Bottle arguments """
+        bottle_params = {
+            'host': config.listen.ip,
+            'port': config.listen.port,
+            'quiet': True
+        }
+    except KeyError:
+        logger.fatal("Config is missing something in the 'listen' section. Use 'config.yml.sample' as guide.")
+        sys.exit(40)
+
+    if 'paths' not in config or not config.paths:
+        logger.fatal("No paths found in config. Use 'config.yml.sample' as guide.")
+        sys.exit(30)
+    for app in config.paths:
+        for app_path, app_config in app.items():
+            if 'auto' not in app_config or not app_config['auto']:
+                if not os.path.isdir(app_path):
+                    logger.warning("Static path '%s' is not a directory. Ignoring." % app_path)
+                    continue
+                if len(app_config['repo']) == 0:
+                    logger.warning("Static path '%s' has no repo configured. Ignoring." % app_path)
+                    continue
+                repo_key = app_config['repo']
+                try:
+                    repo_key += ":" + app_config['branch']
+                except KeyError:
+                    pass
+
+                app_config['basedir'] = app_path
+                REPOS[repo_key] = app_config
+            else:
+                repo_key = app_path
+                app_config['basedir'] = app_path
+                AUTO_REPOS[repo_key] = app_config
+
+    logger.debug('Found %d static projects and %d auto.' % (len(REPOS), len(AUTO_REPOS)))
+
+    """ DEBUG """
+    if 'debug' in config and config.debug:
+        bottle_params['quiet'] = False
+        bottle_params['debug'] = True
+    """ Start listener """
+    bottle.run(**bottle_params)
+
+
+def get_matching_projects(pushed_repo, pushed_branch):
+    """
+    Return a list containing paths matching the pushed repo and branch
+    :param pushed_repo:
+    :param pushed_branch:
+    :return:
+    """
+    pushed_fullname = '%s:%s' % (pushed_repo, pushed_branch)
+    matched = []
+    if pushed_fullname in REPOS and check_match(REPOS[pushed_fullname], pushed_repo, pushed_branch):
+        matched.append(REPOS[pushed_fullname])
+    if pushed_repo in REPOS and check_match(REPOS[pushed_repo], pushed_repo, pushed_branch):
+        matched.append(REPOS[pushed_repo])
+
+    matched += get_matching_in_auto_mode(pushed_repo, pushed_branch)
+    return matched
+
+
+def get_matching_in_auto_mode(repo, branch):
+    """
+    Try to guess project dir by using repo and branch names
+    Paths tested:
+    - repo/
+    - repo/branch/
+    - repo_branch/
+    - repo-branch/
+    - branch/
+
+    :param repo:
+    :param branch:
+    :return:
+    """
+    repo_config = {
+        "repo": repo,
+        "auto": True,
+        "branch": branch,
+        "basedir": "",
+    }
+
+    try:
+        path_possibilities = config.path_possibilities
+    except KeyError:
+        path_possibilities = ['{repo}', '{reposlug}-{branchslug}', '{branchslug}', '{repo}/{branch}', '{repo}_{branch}',
+                              '{repo}-{branch}', '{branch}']
+    path_possibilities = [c.format(repo=repo, branch=branch, reposlug=slugify(repo), branchslug=slugify(branch))
+                          for c in path_possibilities]
+
+    matches = []
+    for auto_path, auto_config in AUTO_REPOS.items():
+        for path in path_possibilities:
+            if os.path.isdir(os.path.join(auto_path, path)):
+                repocheck = repo_config.copy()
+                repocheck.update(auto_config)
+                repocheck['basedir'] = os.path.join(auto_path, path)
+                if check_match(repocheck, repo, branch):
+                    matches.append(repocheck)
+    return matches
+
+
+def check_match(configured_repo, pushed_repo, pushed_branch):
+    """
+    Checks if the repo and branch match the configuration
+
+    :param configured_repo:
+    :param pushed_repo:
+    :param pushed_branch:
+    :return:
+    """
+    try:
+        r = Repo(configured_repo['basedir'])
+        repo_url = r.remotes.origin.config_reader.get('url')
+        repo_name = repo_url.split('/')[-1]
+        if '.git' in repo_name:
+            repo_name = repo_name[:-4]
+        if repo_name != pushed_repo:
+            logger.debug("Repo name mismatch: %s and %s" % (pushed_repo, repo_name))
+            return False
+        """ Get active branch """
+        branch_name = r.active_branch.name
+        if pushed_branch == branch_name:
+            logger.debug("Matching %s" % configured_repo['basedir'])
+            return True
+        else:
+            raise Exception(
+                'Branches are not corresponding: "%s" (local) and "%s" (remote). Skipping.' % (
+                    branch_name, pushed_branch))
+
+    except Exception as e:
+        logger.debug("Check match exception for '%s': %s" % (configured_repo['basedir'], str(e)))
+        return False
+
+
+def update_project(configured_repo):
+    """
+    Updates the given paths
+
+    :param configured_repo:
+    :return:
+    """
+    logger.debug("Updating %s" % configured_repo['basedir'])
+    r = Repo(configured_repo['basedir'])
+
+    try:
+        execute_command(configured_repo['run_before'])
+    except Exception:
+        pass
+    r.remotes.origin.pull()
+    try:
+        execute_command(configured_repo['run_after'])
+    except Exception:
+        pass
+
+
+def execute_command(command):
+    """
+    Used for running app pre/post commands
+    :param command: the command to run
+    """
+    if isinstance(command, str):
+        command = command.split(' ')
+    from subprocess import check_output
+
+    logger.debug('Executing %s' % ' '.join(command))
+    try:
+        out = check_output(command, stderr=STDOUT)
+        logger.debug('Output: %s' % out)
+    except CalledProcessError as e:
+        logger.debug('Error executing %s: %s' % (' '.join(command), e.output))
--- pullhook.py
+++ pullhook.py
@@ -5,85 +5,13 @@
         Feel free to bind the server to any ip and port you desire
         just make sure it's accessible by GitHub
 """
-__author__ = 'talpah@gmail.com'
-
-from ConfigParser import SafeConfigParser
-import logging
 import bottle
 from tendo import singleton
-import git
-import os
 
+from lib import logger, run_application, update_project, \
+    get_matching_projects
 
-MYDIR = os.path.dirname(os.path.abspath(__file__))
-
-"""
- DEFAULT CONFIG VALUES begin
- Use config.ini to override
-"""
-DEFAULT_CONFIG = {
-    'debug': 'false',  # Single instance
-    'listen_host': '0.0.0.0',  # IP to listen on; Defaults to all interfaces
-    'listen_port': '7878',  # Port to listen to
-}
-
-MAIN_CONFIG = {}
-REPOS_CONFIG = {}
-
-"""
-    Single instance
-    Hide warning to prevent cron mails
-"""
-tmp = logging.getLogger().level
-logging.getLogger().setLevel(logging.CRITICAL)
-_myinstance_ = singleton.SingleInstance()
-logging.getLogger().setLevel(tmp)
-
-""" Logging """
-logger = logging.getLogger("pullhook")
-logger.addHandler(logging.StreamHandler())
-
-""" Code below """
-
-
-def init():
-    """
-    Read config file
-
-    """
-    global MAIN_CONFIG
-    config_file = os.path.join(MYDIR, 'config.ini')
-    if not os.path.isfile(config_file):
-        raise Exception('Config file "%s" not found. Exiting.' % config_file)
-    else:
-        parser = SafeConfigParser()
-        parser.read(config_file)
-        MAIN_CONFIG = dict(DEFAULT_CONFIG.items() + dict(parser.items('pullhook')).items())
-        if MAIN_CONFIG['debug'].lower() in ['true', 'yes', '1', 'on']:
-            logger.setLevel(logging.DEBUG)
-        sections = parser.sections()
-        if len(sections) < 2:
-            raise Exception(
-                'Configuration error: No applications defined. Create "config.ini". See "config.ini.sample" for howto.')
-        global REPOS_CONFIG
-        for app_name in sections:
-            if app_name != 'pullhook':
-                config = dict(parser.items(app_name))
-                if 'basedir' not in config:
-                    raise Exception(
-                        'Configuration error: "%s" application is missing the "basedir" key.' % app_name)
-                REPOS_CONFIG[app_name] = config
-        logger.debug('Found %d repos in config.' % len(REPOS_CONFIG))
-
-
-def run_command(command):
-    if isinstance(command, str):
-        command = command.split(' ')
-    from subprocess import check_output
-
-    logger.debug('Running %s' % ' '.join(command))
-    out = check_output(command)
-    logger.debug('Output: %s' % out)
+__author__ = 'talpah@gmail.com'
 
 
 @bottle.route('/', method=['POST'])
@@ -95,56 +23,35 @@ def handle_payload():
     data = bottle.request.json
     event = bottle.request.get_header('X-GitHub-Event')
     logger.debug("Received event: %s" % event)
+
     if event == 'push':
         """ Parse repo name from push data ref """
         pushed_repo = data['repository']['name']
         """ Parse branch name from push data ref """
-        pushed_branch = data['ref'].split('/')[-1]
+        pushed_branch = data['ref'].split('/', 2)[-1]
 
-        pushed_w_branch = '%s:%s' % (pushed_repo, pushed_branch)
         logger.debug("Received push event from repo %s in branch %s" % (pushed_repo, pushed_branch))
 
-        if pushed_w_branch in REPOS_CONFIG:
-            configured_repo = REPOS_CONFIG[pushed_w_branch]
-        elif pushed_repo in REPOS_CONFIG:
-            configured_repo = REPOS_CONFIG[pushed_repo]
-        else:
-            logger.warning('Repo "%s" is not configured on our end. Skipping.' % pushed_repo)
-            bottle.abort(404, 'Not configured: %s' % pushed_repo)
-            return
+        matching_projects = get_matching_projects(pushed_repo, pushed_branch)
 
-        """ Use GitPython """
-        g = git.Git(configured_repo['basedir'])
-        """ Get active branch """
-        branch = g.rev_parse('--abbrev-ref', 'HEAD')
-        logger.debug("Repo \"%s\", branch \"%s\"" % (configured_repo['basedir'], branch))
-        if pushed_branch == branch:
-            logger.debug("Pulling new commits.")
-            g.pull()
-            if 'run' in configured_repo:
-                try:
-                    run_command(configured_repo['run'])
-                except Exception:
-                    pass
-        else:
-            logger.debug(
-                'Branches are not corresponding: "%s" (local) and "%s" (remote). Skipping.' % (branch, pushed_branch))
-            # no abort here
+        if not matching_projects:
+            logger.debug('Push for "%s/%s" did not match anything. Skipping.' % (pushed_repo, pushed_branch))
+            bottle.abort(404, 'Not configured: %s/%s' % (pushed_repo, pushed_branch))
+
+        for m in matching_projects:
+            update_project(m)
 
 
 if __name__ == '__main__':
-    init()
+    """
+        Single instance
+        Hide warnings to prevent cron mails
+    """
+    import logging
 
-    """ Bottle arguments """
-    bottle_params = {
-        'host': MAIN_CONFIG['listen_host'],
-        'port': MAIN_CONFIG['listen_port'],
-        'quiet': True
-    }
+    tmp = logging.getLogger().level
+    logging.getLogger().setLevel(logging.CRITICAL)
+    _myinstance_ = singleton.SingleInstance()
+    logging.getLogger().setLevel(tmp)
 
-    """ DEBUG """
-    if MAIN_CONFIG['debug'].lower() in ['true', 'yes', '1', 'on']:
-        bottle_params['quiet'] = False
-        bottle_params['debug'] = True
-
-    bottle.run(**bottle_params)
+    run_application()
